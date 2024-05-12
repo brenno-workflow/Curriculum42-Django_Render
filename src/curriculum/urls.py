72dc306cf9dd0d2e96e7158bb3178b017f9b0b09
Brenno Brossi <brenno_workflow@outlook.com>
2024-05-12 06:26:55 -0300

    T2 - Published, list and acess

--- src/curriculum/urls.py
+++ src/curriculum/urls.py
@@ -10,9 +10,14 @@ urlpatterns = [
     path('accessLevel/<int:id>', views.accessLevel, name='accessLevel'),
     path('published/<int:id>', views.published, name='published'),
     path('profile/<int:id>', views.profile, name='profile'),
+
+    # News
     path('create_key/<int:id>', views.create_key, name='create_key'),
     re_path('profile_key/(?P<id>\d+)/$', views.profile_key, name='profile_key'),
     re_path('update_key/(?P<id>\d+)$', views.update_key, name='update_key'),
-    re_path('delete_key/(?P<id>\d+)$', views.delete_key, name='delete_key')
+    re_path('delete_key/(?P<id>\d+)$', views.delete_key, name='delete_key'),
+    path('list_key/<int:id>', views.list_key, name='list_key'),
+    re_path('accessLevel_key/(?P<id>\d+)$', views.accessLevel_key, name='accessLevel_key'),
+    re_path('published_key/(?P<id>\d+)$', views.published_key, name='published_key')
 ]
 
